2d69b4c7628618a69fc05e4234f147398f2798b9
Brian <gisameht@gmail.com>
2024-02-19 21:03:11 -0500

    recomendacion a partir de varios libros

--- src/project_code/code.py
+++ src/project_code/code.py
@@ -30,6 +30,8 @@ Execution Steps:
 6. Measure and display the execution time.
 """
 
+# Measure start time
+start_time = time.time()
 # Preprocessing data
 preprocess = PreprocessingUtils()
 
@@ -59,8 +61,6 @@ else:
     # Guardar datos preprocesados
     df.to_csv(enviroments.completedPath, index=False)
 
-# Measure start time
-start_time = time.time()
 
 # Text modeling, similarity modeling, and recommendation system
 text_model = TfidfModel()
@@ -72,127 +72,11 @@ recommendation_system = RecommendationSystem(similarity_model)
 indices = pd.Series(df.index, index=df["title"]).to_dict()
 
 # Generate and print recommendations
-recommended_books = recommendation_system.get_recommendations(
-    "The Scarlet Letter A Romance", indices, df
+recommended_books = recommendation_system.get_recommendations_by_user(
+    user_id="A252PRC1XBMTQJ", user_books_df=df, indices=indices, df=df
 )
 print(recommended_books)
 
 # Measure and print execution time
 end_time = time.time()
 print("Tiempo de ejecución:", end_time - start_time, "segundos")
-
-# ===================================
-# Verificar si el archivo preprocesado existe
-# if os.path.exists(enviroments.completedPath):
-#     # Cargar el archivo preprocesado
-#     df = pd.read_csv(enviroments.completedPath)
-# else:
-#     # Cargar y preprocesar los datos
-
-#     # Cargar datos
-#     df = pd.read_csv(enviroments.rawPaht)
-#     df.rename(
-#         columns={
-#             "Id": "book_id",
-#             "User_id": "user_id",
-#             "review/text": "review",
-#             "Title": "title",
-#             "review/score": "rating",
-#         },
-#         inplace=True,
-#     )
-
-#     df = df.head(10000)
-#     print(df.head())
-
-#     # Limpieza de texto
-#     def clean_text(text):
-#         text = str(text).lower()
-#         text = re.sub("\[.*?\]", "", text)
-#         text = re.sub("https?://\S+|www\.\S+", "", text)
-#         text = re.sub("<.*?>+", "", text)
-#         text = re.sub("[%s]" % re.escape(string.punctuation), "", text)
-#         text = re.sub("\n", "", text)
-#         text = re.sub("\w*\d\w*", "", text)
-#         return text
-
-#     # Verificar si los stopwords ya están descargados
-#     try:
-#         stop_words = set(stopwords.words("english"))
-#     except LookupError:
-#         nltk.download("stopwords")
-#         stop_words = set(stopwords.words("english"))
-
-#     # Inicializar el stemmer solo si no existe
-#     if "stemmer" not in globals():
-#         stemmer = SnowballStemmer("english")
-
-#     def preprocess_data(text):
-#         text = clean_text(text)
-#         text = " ".join(word for word in text.split() if word not in stop_words)
-#         text = " ".join(stemmer.stem(word) for word in text.split())
-#         return text
-
-#     # Combinar título y reseña en una nueva columna
-#     df["combined_features"] = df["title"] + " " + df["review"]
-#     df["combined_features"] = df["combined_features"].fillna("")
-
-#     # Aplicar preprocesamiento a la columna combinada
-#     df["combined_features_clean"] = df["combined_features"].apply(preprocess_data)
-
-#     # Guardar datos preprocesados
-#     df.to_csv(enviroments.completedPath, index=False)
-
-# start_time = time.time()
-
-
-# # Crear o cargar la matriz TF-IDF
-# if os.path.exists(enviroments.tfidf_matrix_file):
-#     with open(enviroments.tfidf_matrix_file, "rb") as f:
-#         tfidf_matrix_reduced = pickle.load(f)
-# else:
-#     tfidf = TfidfVectorizer(max_features=1000, ngram_range=(1, 2))
-#     tfidf_matrix = tfidf.fit_transform(df["combined_features_clean"])
-#     svd = TruncatedSVD(n_components=100)
-#     tfidf_matrix_reduced = svd.fit_transform(tfidf_matrix)
-#     with open(enviroments.tfidf_matrix_file, "wb") as f:
-#         pickle.dump(tfidf_matrix_reduced, f)
-
-# # Crear o cargar la matriz de similitud del coseno
-# if os.path.exists(enviroments.cosine_sim_file):
-#     with open(enviroments.cosine_sim_file, "rb") as f:
-#         cosine_sim = pickle.load(f)
-# else:
-#     cosine_sim = linear_kernel(tfidf_matrix_reduced, tfidf_matrix_reduced)
-#     with open(enviroments.cosine_sim_file, "wb") as f:
-#         pickle.dump(cosine_sim, f)
-
-
-# indices = pd.Series(df.index, index=df["title"]).to_dict()
-
-
-# def get_recommendations(title, cosine_sim=cosine_sim, indices=indices):
-#     idx = indices.get(title)
-#     if idx is None:
-#         raise ValueError("Title not found in dataset")
-#     # Obtiene el book_id del libro original
-#     book_id_original = df.at[idx, "book_id"]
-#     print("Book id original:", book_id_original)
-
-#     sim_scores = list(enumerate(cosine_sim[idx]))
-#     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-#     # Filtrar para excluir el libro original por book_id
-#     book_indices = [
-#         i[0] for i in sim_scores if df.at[i[0], "book_id"] != book_id_original
-#     ]
-
-#     # Obtiene los 10 libros más similares
-#     book_indices = book_indices[0:10]
-#     return df.iloc[book_indices].drop(
-#         columns=["combined_features_clean", "combined_features"]
-#     )
-
-
-# print(get_recommendations("The Scarlet Letter A Romance"))
-# end_time = time.time()
-# print("Tiempo de ejecución:", end_time - start_time, "segundos")
--- src/project_code/models/recommendation_system.py
+++ src/project_code/models/recommendation_system.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class RecommendationSystem:
     """
     A class to represent a recommendation system.
@@ -20,7 +23,7 @@ class RecommendationSystem:
         """
         self.similarity_model = similarity_model
 
-    def get_recommendations(self, title, indices, df):
+    def get_recommendation(self, title, indices, df):
         """
         Generates a list of book recommendations based on the provided title.
 
@@ -58,5 +61,73 @@ class RecommendationSystem:
 
         # Return the recommended book data
         return df.iloc[book_indices].drop(
+            columns=["combined_features_clean", "combined_features"]
+        )
+
+    def get_recommendations_by_user(self, user_id, user_books_df, indices, df):
+        """
+        Generates a list of book recommendations for a given user ID.
+
+        Args:
+            user_id (int): The ID of the user for whom recommendations are to be made.
+            user_books_df (DataFrame): The dataframe containing user-book associations.
+            indices (dict): A dictionary mapping titles to their respective indices in the dataset.
+            df (DataFrame): The dataframe containing book data.
+
+        Returns:
+            DataFrame: A DataFrame of the top recommended books, excluding the books read by the user.
+
+        Raises:
+            ValueError: If the user ID is not found in the dataset.
+        """
+
+        if user_id not in user_books_df["user_id"].values:
+            raise ValueError("User ID not found in dataset")
+        user_books = df[df["user_id"] == user_id]
+        user_books_titles = user_books["title"].tolist()
+        # Get all books read by the user
+        # user_books = user_books_df[user_books_df["user_id"] == user_id][
+        #     "book_id"
+        # ].tolist()
+
+        # # Convert book IDs to titles
+        # user_books_titles = df[df["book_id"].isin(user_books)]["title"].tolist()
+
+        # # Ensure that the books are in the indices dictionary
+        # user_books_titles = [title for title in user_books_titles if title in indices]
+
+        # Now use the existing logic to get recommendations based on these titles
+        return self.get_recommendations(user_books_titles, indices, df)
+
+    def get_recommendations(self, titles, indices, df):
+        total_sim_scores = np.zeros(len(df))  # Inicializar un array de ceros
+        print(titles)
+
+        for title in titles:
+            if title not in indices:
+                raise ValueError(f"Title '{title}' not found in dataset")
+
+            idx = indices[title]
+            sim_scores = self.similarity_model.get_similarity_scores(idx)
+
+            # Sumar las puntuaciones de similitud para cada libro en el DataFrame
+            for i, score in sim_scores:
+                total_sim_scores[i] += score
+
+        # Calcular el promedio de las puntuaciones de similitud
+        avg_sim_scores = total_sim_scores / len(total_sim_scores)
+
+        # Ordenar los índices de los libros según las puntuaciones de similitud en orden descendente
+        sorted_indices = np.argsort(avg_sim_scores)[::-1]
+
+        # Excluir los libros originales y obtener los índices de los libros similares
+        excluded_indices = [indices[title] for title in titles]
+        filtered_indices = [i for i in sorted_indices if i not in excluded_indices]
+
+        # Seleccionar los índices de los libros más similares
+        top_book_indices = filtered_indices[:10]
+
+        # Devolver los datos de los libros recomendados
+        return df.iloc[top_book_indices].drop(
             columns=["combined_features_clean", "combined_features"]
         )
